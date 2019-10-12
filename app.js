9160f5a886349b88b8cc47cc040ac9d9722db16a
Chad Chaney <cchane4@gmail.com>
2019-10-12 13:04:57 -0500

    adding friends.ejs and home.ejs

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 let express = require("express"); 
 let app = express(); 
 app.set('view engine', "ejs"); 
+
+app.get("/",(req,res) => { 
+    res.render("home"); 
+}); 
+
+app.get("/friends", (req, res) => { 
+    let friend = ["Tony", "Miranda", "Chad"]; 
+    res.render('friends', {friends_listing: friend}); 
+}); 
+
+app.post("/addfriend", (req, res) => { 
+    console.log(req.body); 
+    res.send("you have reached the post route");
+
+});
+
 app.listen(3000,() => { 
     console.log('server is listening'); 
 
 }); 
+
